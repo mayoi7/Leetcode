78037c0c018818abb68602490e485d0e66fc4eb7
mayoi7 <3451669860@qq.com>
2018-11-03 18:31:20 +0800

    N.103 searchMatrix

--- Leetcode/solution1.h
+++ Leetcode/solution1.h
@@ -428,3 +428,23 @@ public:
 		}
 	}
 };
+
+// ËÑË÷¶þÎ¬¾ØÕó II
+// https://leetcode-cn.com/explore/interview/card/top-interview-questions-medium/50/sorting-and-searching/103/
+class Solution11 {
+public:
+	bool searchMatrix(vector<vector<int>>& matrix, int target) {
+		int raw = matrix.size();
+		if (raw == 0) return false;
+		int col = matrix[0].size();
+		int x = 0, y = col - 1;
+		if (raw == 0 || col == 0) return false;
+		while (x < raw && y >= 0) {
+			if (matrix[x][y] == target) return true;
+			else if (matrix[x][y] < target) ++x;
+			else if (matrix[x][y] > target) --y;
+		}
+		return false;
+	}
+};
+
